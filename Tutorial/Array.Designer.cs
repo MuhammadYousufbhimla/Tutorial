9380a54e610a2f21df11e0cf893811e733f78f57
Muhammad Yousuf <Muhammadyousufgemini@gmail.com>
2023-03-02 14:37:13 +0500

    Array work Student Record

---
+++ Tutorial/Array.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Tutorial
+{
+    partial class Array
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button3 = new Button();
+            txtfees = new TextBox();
+            txtage = new TextBox();
+            txtgender = new TextBox();
+            txtfname = new TextBox();
+            txtname = new TextBox();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            button2 = new Button();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button3
+            // 
+            button3.Location = new Point(304, 232);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 26;
+            button3.Text = "Clear";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // txtfees
+            // 
+            txtfees.Location = new Point(198, 176);
+            txtfees.Name = "txtfees";
+            txtfees.Size = new Size(136, 23);
+            txtfees.TabIndex = 25;
+            // 
+            // txtage
+            // 
+            txtage.Location = new Point(198, 130);
+            txtage.Name = "txtage";
+            txtage.Size = new Size(136, 23);
+            txtage.TabIndex = 24;
+            // 
+            // txtgender
+            // 
+            txtgender.Location = new Point(198, 95);
+            txtgender.Name = "txtgender";
+            txtgender.Size = new Size(136, 23);
+            txtgender.TabIndex = 23;
+            // 
+            // txtfname
+            // 
+            txtfname.Location = new Point(198, 58);
+            txtfname.Name = "txtfname";
+            txtfname.Size = new Size(136, 23);
+            txtfname.TabIndex = 22;
+            // 
+            // txtname
+            // 
+            txtname.Location = new Point(198, 25);
+            txtname.Name = "txtname";
+            txtname.Size = new Size(136, 23);
+            txtname.TabIndex = 21;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(62, 184);
+            label5.Name = "label5";
+            label5.Size = new Size(33, 15);
+            label5.TabIndex = 20;
+            label5.Text = "Fees:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(62, 138);
+            label4.Name = "label4";
+            label4.Size = new Size(31, 15);
+            label4.TabIndex = 19;
+            label4.Text = "Age:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(62, 103);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 15);
+            label3.TabIndex = 18;
+            label3.Text = "Gender:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(62, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 17;
+            label2.Text = "F_name:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(62, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 16;
+            label1.Text = "Name:";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(198, 232);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 15;
+            button2.Text = "Display";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(62, 232);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 14;
+            button1.Text = "Add";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Array
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(540, 327);
+            Controls.Add(button3);
+            Controls.Add(txtfees);
+            Controls.Add(txtage);
+            Controls.Add(txtgender);
+            Controls.Add(txtfname);
+            Controls.Add(txtname);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Array";
+            Text = "Array";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button3;
+        private TextBox txtfees;
+        private TextBox txtage;
+        private TextBox txtgender;
+        private TextBox txtfname;
+        private TextBox txtname;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button button2;
+        private Button button1;
+    }
+}
---
+++ Tutorial/Array.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutorial
+{
+    public partial class Array : Form
+    {
+        String[] name = new String[2];
+        String[] fname = new String[2];
+        String[] age = new String[2];
+        String[] gender = new String[2];
+        String[] fees = new String[2];
+
+        String Recordcount = "1";
+        String Recorddisplay = "1";
+
+        public Array()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Recordcount == "1")
+            {
+                name[0] = txtname.Text;
+                fname[0] = txtfname.Text;
+                age[0] = txtage.Text;
+                fees[0] = txtfees.Text;
+                gender[0] = txtgender.Text;
+                MessageBox.Show("Add First Record");
+
+                Recordcount = "2";
+            }
+            else if (Recordcount == "2")
+            {
+                name[1] = txtname.Text;
+                fname[1] = txtfname.Text;
+                age[1] = txtage.Text;
+                fees[1] = txtfees.Text;
+                gender[1] = txtgender.Text;
+                MessageBox.Show("Add second Record");
+            }
+            else
+            {
+                MessageBox.Show("Array Limit Finished");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Recorddisplay == "1")
+            {
+                txtname.Text = name[0];
+                txtfname.Text = fname[0];
+                txtage.Text = age[0];
+                txtfees.Text = fees[0];
+                txtgender.Text =gender[0];
+
+                Recorddisplay = "2";
+
+            }
+            else if (Recorddisplay == "2")
+            {
+                txtname.Text = name[1];
+                txtfname.Text = fname[1];
+                txtage.Text = age[1];
+                txtfees.Text = fees[1];
+                txtgender.Text = gender[1];
+            }
+            else
+            {
+                MessageBox.Show("No Record Found");
+            }
+        }
+    }
+}
